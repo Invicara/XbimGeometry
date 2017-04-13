8464670e8a0b264557cac80d997ec61f08d23295
inv-WawanSolihin <wawan.solihin@invicara.com>
2017-04-13 21:35:56 +0800

    Minor changes for release builds

--- Publish.bat
+++ Publish.bat
@@ -1,4 +1,4 @@
-"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" Xbim.Geometry.Nuget.sln /build Release
+"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Geometry.Nuget.sln" /build "Release|x64"
 nuget pack Xbim.Geometry.Nuspec
 
 copy *.nupkg ..\LocalPackages
