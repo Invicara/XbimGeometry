9aec82031900b9d6ede1d4fe0b9b2954a3b8605b
inv-WawanSolihin <wawan.solihin@invicara.com>
2017-04-25 00:03:58 +0800

    - Change config to use only x64

--- Publish-pdb.bat
+++ Publish-pdb.bat
@@ -4,7 +4,7 @@ nuget restore "Xbim.Geometry.Nuget.sln"
 sleep 2
 nuget restore "Xbim.Geometry.Nuget.sln"
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Geometry.Nuget.sln" /build "ReleaseWithPDB|Any CPU"
+"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Geometry.Nuget.sln" /build "ReleaseWithPDB|x64"
 nuget pack Xbim.Geometry.Nuspec
 
 copy *.nupkg ..\LocalPackages
