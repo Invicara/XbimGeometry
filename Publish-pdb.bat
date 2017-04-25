a6d56377df403cd98871e081b7a32afcb1946204
inv-WawanSolihin <wawan.solihin@invicara.com>
2017-04-25 16:00:03 +0800

    Batch file to build Release version with debugable information and an option for different cpu type

--- Publish-pdb.bat
+++ Publish-pdb.bat
@@ -4,7 +4,10 @@ nuget restore "Xbim.Geometry.Nuget.sln"
 sleep 2
 nuget restore "Xbim.Geometry.Nuget.sln"
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Geometry.Nuget.sln" /build "ReleaseWithPDB|x64"
+set cputype=%~1
+if "%~1" == "" set cputype=x64
+
+"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Geometry.Nuget.sln" /build "ReleaseWithPDB|%cputype%"
 nuget pack Xbim.Geometry.Nuspec
 
 copy *.nupkg ..\LocalPackages
