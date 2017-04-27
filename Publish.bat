91a982f89ce65314f1f04bf8dd0d0721e76eb275
inv-WawanSolihin <wawan.solihin@invicara.com>
2017-04-27 15:18:47 +0800

    Add clean operation

--- Publish.bat
+++ Publish.bat
@@ -4,8 +4,12 @@ nuget restore "Xbim.Geometry.Nuget.sln"
 sleep 2
 nuget restore "Xbim.Geometry.Nuget.sln"
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Geometry.Nuget.sln" /build "Release|x64"
-nuget pack Xbim.Geometry.Nuspec
+set buildConfig="%~1"
+if "%~1" == "" set buildConfig="Release|Any CPU"
 
+"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Geometry.Nuget.sln" /clean %buildConfig%
+"%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" "Xbim.Geometry.Nuget.sln" /build %buildConfig%
+
+nuget pack Xbim.Geometry.Nuspec
 copy *.nupkg ..\LocalPackages
 del *.nupkg
